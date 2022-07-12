040a30b6e20de09055390d2def3b7363e67981bc
Дмитрий <87432204+2Astrel4@users.noreply.github.com>
2022-07-12 16:43:30 +0400

    Добавил цикл while

--- Program.cs
+++ Program.cs
@@ -4,9 +4,15 @@
     {
         static void Main(string[] args)
         {
+            int i = 0;
             string name = "Astrel";
             Console.WriteLine("Hello, World!" + Environment.NewLine);
             Console.WriteLine(name);
+            while (i < 10)
+            {
+                Console.WriteLine("!");
+                i++;
+            }
         }
     }
 }

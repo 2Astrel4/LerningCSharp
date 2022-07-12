95238e7dd9efb3a0fc9dd5810839fc22ec5d3d44
Дмитрий <87432204+2Astrel4@users.noreply.github.com>
2022-07-12 17:54:23 +0400

    Commit проекта WinForm
    Проект выводит "Фрактальное дерево" в оконное приложение Windows

---
+++ Проект-2/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Fraktal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1842, 963);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1842, 963);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Проект-2/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fraktal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void drawF(int x, int y, int len, double rotation, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            double x1, y1;
+            x1 = x + len * Math.Sin(rotation * Math.PI * 2 / 270.0);
+            y1 = y + len * Math.Cos(rotation * Math.PI * 2 / 270.0);
+            g.DrawLine(new Pen(Color.CadetBlue, len / 8), x,panel1.Height- y, (int)x1, panel1.Height-(int)y1);
+            if (len > 1)
+            {
+                drawF((int)x1, (int)y1, (int)(len / 1.35), (int)rotation + 30, e);
+                drawF((int)x1, (int)y1, (int)(len / 1.35), (int)rotation - 30, e);
+            }
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            drawF(panel1.Width / 2, 0, 150, 0, e);
+            drawF(panel1.Width / 2, 0, 100, 0, e);
+            drawF(panel1.Width / 2, 0, 50, 0, e);
+        }
+    }
+}

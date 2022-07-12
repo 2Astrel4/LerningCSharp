dd86496c5950553486ad1141031bf98f5337e277
Дмитрий <87432204+2Astrel4@users.noreply.github.com>
2022-07-12 17:51:16 +0400

    Commit файлов
    Program.cs | AkademyCifra.csproj | AkademyCifra.sln

---
+++ Проект-1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace AkademyCifra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            string name = "Astrel";
+            Console.WriteLine("Hello, World!" + Environment.NewLine);
+            Console.WriteLine(name);
+            while (i < 10)
+            {
+                Console.WriteLine("!");
+                i++;
+            }
+        }
+    }
+}
